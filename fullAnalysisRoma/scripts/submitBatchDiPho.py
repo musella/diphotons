fc5de3f5905d5a4386510039e510a415f4a51b52
crovelli <crovelli@gmail.com>
2015-07-29 18:37:06 +0200

    updated samples

--- fullAnalysisRoma/scripts/submitBatchDiPho.py
+++ fullAnalysisRoma/scripts/submitBatchDiPho.py
@@ -43,12 +43,9 @@ def main():
     xsection = args[4]
     kfactor = args[5]
 
-    if (sampleIndex<10000):
-        inputlist="lists_Spring15v1/50ns/MC/"+dataset+".list"
-    else: inputlist="lists_Spring15v1/50ns/data/"+dataset+".list" 
-    if (sampleIndex<10000):
-        inputweights="lists_Spring15v1/50ns/MC/"+dataset+".weight"
-    else: inputweights="lists_Spring15v1/50ns/data/"+dataset+".weight"
+    inputlist="lists_Spring15v1/25ns/MC/"+dataset+".list"
+    inputweights="lists_Spring15v1/25ns/MC/"+dataset+".weight"
+
     output = dataset
 
     print "the outputs will be in the directory: "+opt.prefix
