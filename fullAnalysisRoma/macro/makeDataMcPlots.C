d76dba2a8b0c2d4b5433cca9b13713959450d0b8
crovelli <crovelli@gmail.com>
2015-07-30 23:14:42 +0200

    more plots

--- fullAnalysisRoma/macro/makeDataMcPlots.C
+++ fullAnalysisRoma/macro/makeDataMcPlots.C
@@ -13,7 +13,7 @@
 #include <iostream>
 
 #define NSPECIES 5
-#define NVARIABLES 1
+#define NVARIABLES 18
 #define NCUTS 1
 
 void makeDataMcPlots(float lumi, bool blindData=false, int signalFactor=1)
@@ -46,11 +46,11 @@ void makeDataMcPlots(float lumi, bool blindData=false, int signalFactor=1)
 
   // chiara
   TString files[NSPECIES];
-  files[0]="data/mergedFinal/DoubleEG.root";
-  files[1]="data/mergedFinal/RSGravToGG_kMpl-01_M-1000.root";
-  files[2]="data/mergedFinal/QCD.root";
-  files[3]="data/mergedFinal/GJets.root";
-  files[4]="data/mergedFinal/GGJets.root";
+  files[0]="data/noHLT/mergedFinal/DoubleEG.root";
+  files[1]="data/noHLT/mergedFinal/RSGravToGG_kMpl-01_M-1000.root";
+  files[2]="data/noHLT/mergedFinal/QCD.root";
+  files[3]="data/noHLT/mergedFinal/GJets.root";
+  files[4]="data/noHLT/mergedFinal/GGJets.root";
 
   TString plotsDir="./diphotPlots/";
 
@@ -62,7 +62,6 @@ void makeDataMcPlots(float lumi, bool blindData=false, int signalFactor=1)
   // chiara
   TString variables[NVARIABLES];
   variables[0]="mgg";
-  /*
   variables[1]="ptgg";
   variables[2]="pt1";
   variables[3]="pt2";
@@ -70,49 +69,79 @@ void makeDataMcPlots(float lumi, bool blindData=false, int signalFactor=1)
   variables[5]="eta2";
   variables[6]="r91";
   variables[7]="r92";
-  */
+  variables[8]="nvtx";
+  variables[9]="eventClass";
+  variables[10]="chiso1";
+  variables[11]="chiso2";
+  variables[12]="phoiso1";
+  variables[13]="phoiso2";
+  variables[14]="sieie1";
+  variables[15]="sieie2";
+  variables[16]="hoe1";
+  variables[17]="hoe2";
 
   // chiara
   TString units[NVARIABLES];
-  units[0]="GeV/c^{2}";
-  /*
-  units[1]="GeV/c";
-  units[2]="GeV/c";
-  units[3]="GeV/c";
+  units[0]="GeV";
+  units[1]="GeV";
+  units[2]="GeV";
+  units[3]="GeV";
   units[4]="";
   units[5]="";
   units[6]="";
   units[7]="";
-  */
+  units[8]="";
+  units[9]="";
+  units[10]="GeV";
+  units[11]="GeV";
+  units[12]="GeV";
+  units[13]="GeV";
+  units[14]="";
+  units[15]="";
+  units[16]="";
+  units[17]="";
 
   // chiara
   int nbins[NVARIABLES];
-  nbins[0]=100;
-  /*
-  nbins[1]=50;
-  nbins[2]=50;
-  nbins[3]=50;
-  nbins[4]=50;
-  nbins[5]=50;
-  nbins[6]=20;
-  nbins[7]=20;
-  */
+  nbins[0]=30;
+  //nbins[1]=50;
+  nbins[1]=20;
+  // nbins[2]=50;
+  // nbins[3]=50;
+  nbins[2]=20;
+  nbins[3]=20;
+  nbins[4]=20;
+  nbins[5]=20;
+  nbins[6]=10;
+  nbins[7]=10;
+  nbins[8]=20;
+  nbins[9]=4;
+  nbins[10]=10;
+  nbins[11]=10;
+  nbins[12]=10;
+  nbins[13]=10;
+  nbins[14]=50;
+  nbins[15]=50;
+  nbins[16]=20;
+  nbins[17]=20;
 
   // chiara
   float range[NVARIABLES][2]; // N variables, min, max
   // mgg
-  range[0][0]=0.;
-  range[0][1]=1000.;
-  /*
+  range[0][0]=150.;
+  range[0][1]=1050.;
   // ptgg
   range[1][0]=0.;
-  range[1][1]=3000.;
+  // range[1][1]=3000.;
+  range[1][1]=1000.;
   // pt1
   range[2][0]=0.;
-  range[2][1]=2000.;
+  // range[2][1]=2000.;
+  range[2][1]=500.;
   // pt2
   range[3][0]=0.;
-  range[3][1]=1000.;
+  //range[3][1]=1000.;
+  range[3][1]=500.;
   // eta1
   range[4][0]=-3.;
   range[4][1]=3.;
@@ -125,12 +154,40 @@ void makeDataMcPlots(float lumi, bool blindData=false, int signalFactor=1)
   // r91
   range[7][0]=0.8;
   range[7][1]=1.;
-  */
+  // # vtx
+  range[8][0]=0.;
+  range[8][1]=40.;  
+  // # event class
+  range[9][0]=-0.5;
+  range[9][1]=3.5;  
+  // charged isolation 1
+  range[10][0]=0.;
+  range[10][1]=5.;  
+  // charged isolation 2
+  range[11][0]=0.;
+  range[11][1]=5.;  
+  // photon isolation 1
+  range[12][0]=0.;
+  range[12][1]=10.;  
+  // photon isolation 2
+  range[13][0]=0.;
+  range[13][1]=10.;  
+  // sIeIe 1
+  range[14][0]=0.;
+  range[14][1]=0.05;  
+  // sIeIe 2
+  range[15][0]=0.;
+  range[15][1]=0.05;  
+  // H/E 1
+  range[16][0]=0.;
+  range[16][1]=0.05;  
+  // H/E 2
+  range[17][0]=0.;
+  range[17][1]=0.05;  
 
   // chiara
   TString xaxisLabel[NVARIABLES];
   xaxisLabel[0]="m(#gamma#gamma)";
-  /*
   xaxisLabel[1]="p_{T}(#gamma #gamma)";
   xaxisLabel[2]="p_{T}(#gamma1)";
   xaxisLabel[3]="p_{T}(#gamma2)";
@@ -138,7 +195,17 @@ void makeDataMcPlots(float lumi, bool blindData=false, int signalFactor=1)
   xaxisLabel[5]="#eta(#gamma2)";
   xaxisLabel[6]="R9(#gamma1)";
   xaxisLabel[7]="R9(#gamma2)";
-  */
+  xaxisLabel[8]="Vertices";
+  xaxisLabel[9]="Class";
+  xaxisLabel[10]="Charged isolation(#gamma1)";
+  xaxisLabel[11]="Charged isolation(#gamma2)";
+  xaxisLabel[12]="Photon isolation(#gamma1)";
+  xaxisLabel[13]="Photon isolation(#gamma2)";
+  xaxisLabel[14]="sigmaIetaIeta(#gamma1)";
+  xaxisLabel[15]="sigmaIetaIeta(#gamma2)";
+  xaxisLabel[16]="H/E(#gamma1)";
+  xaxisLabel[17]="H/E(#gamma2)";
+
 
   TString binSize[NVARIABLES];
 
@@ -147,8 +214,7 @@ void makeDataMcPlots(float lumi, bool blindData=false, int signalFactor=1)
       sprintf(icut[j],"icut%d",j);
       for (int i=0;i<NSPECIES;++i) {
 	histos[i][j][z]=new TH1F(variables[z]+"_"+species[i]+"_"+TString(icut[j]),variables[z]+"_"+species[i]+"_"+TString(icut[j]),nbins[z],range[z][0],range[z][1]);
-	if(i==0)
-	  histos[i][j][z]->Sumw2();
+	histos[i][j][z]->Sumw2();
 	char binsiz[10];
 	sprintf(binsiz,"%2.0f",(range[z][1]-range[z][0])/nbins[z]);
 	binSize[z]=TString(binsiz);
@@ -156,15 +222,19 @@ void makeDataMcPlots(float lumi, bool blindData=false, int signalFactor=1)
     }
   }
 
+
   // chiara
   TString cut[NCUTS];
-  cut[0]="(pt1>200 && pt2>200 && mgg>500)*";
   /*
+  cut[0]="(pt1>200 && pt2>200 && mgg>500)*";
   cut[1]="(pt1>200 && pt2>200 && mgg>500 && eventClass==0)*";
   cut[2]="(pt1>200 && pt2>200 && mgg>500 && eventClass==1)*";
   cut[3]="(pt1>200 && pt2>200 && mgg>500 && eventClass==2)*";
   cut[4]="(pt1>200 && pt2>200 && mgg>500 && eventClass==3)*";
   */
+  // cut[0]="(pt1>50 && pt2>50 && mgg>50)*";
+  cut[0]="(pt1>50 && pt2>50 && mgg>250)*";
+  // cut[0]="(pt1>100 && pt2>100 && mgg>250)*";
 
   char lumistr[5];
   sprintf(lumistr,"%.1f",lumi);
@@ -172,6 +242,10 @@ void makeDataMcPlots(float lumi, bool blindData=false, int signalFactor=1)
   TFile *_file[NSPECIES];
   TTree *T1[NSPECIES];
   
+  char lumiwgt[10];
+  sprintf(lumiwgt,"%f*",lumi);
+  cout << "lumiwgt = " << lumiwgt << endl;
+
   if(!blindData) {  
     _file[0]=TFile::Open(files[0]);   
     T1[0] = (TTree*)_file[0]->Get("DiPhotonTree");    
@@ -199,7 +273,13 @@ void makeDataMcPlots(float lumi, bool blindData=false, int signalFactor=1)
 	  std::cout << "Species " << i << " Tree not found" << std::endl;
 	  return;
 	}
-	T1[i]->Project(histoName,variables[z],cut[j]+"weight*"+intLumi);
+	if (i>0) {
+	  T1[i]->Project(histoName,variables[z],cut[j]+"weight*"+intLumi);
+	  cout << "non dati: " << cut[j]+"weight*"+intLumi << endl;
+	} else { 
+	  T1[i]->Project(histoName,variables[z],cut[j]+TString("1"));
+	  cout << "dati: " << cut[j]+TString("1") << endl;
+	}
 	std::cout << "Done " << histoName << std::endl;
       }
           
@@ -211,9 +291,13 @@ void makeDataMcPlots(float lumi, bool blindData=false, int signalFactor=1)
       myPlot.setMCHist(iQCD,   histos[2][j][z]);
       myPlot.setMCHist(iGJets, histos[3][j][z]);
       myPlot.setMCHist(iGG,    histos[4][j][z]);
-      myPlot.setMCHist(iRS,    histos[1][j][z]);
+      // myPlot.setMCHist(iRS,    histos[1][j][z]);
 
       if(!blindData) myPlot.setDataHist(histos[0][j][z]);                 
+      cout << "data: " << histos[0][j][z]->GetEntries() << " " << histos[0][j][z]->Integral() << endl;
+      cout << "qcd: "  << histos[2][j][z]->GetEntries() << " " << histos[2][j][z]->Integral() << endl;
+      cout << "gj: "   << histos[3][j][z]->GetEntries() << " " << histos[3][j][z]->Integral() << endl;
+      cout << "gg: "   << histos[4][j][z]->GetEntries() << " " << histos[4][j][z]->Integral() << endl;
 
       // Draw
       //--------------------------------------------------------------------
@@ -223,7 +307,8 @@ void makeDataMcPlots(float lumi, bool blindData=false, int signalFactor=1)
       c1->SetLogy(0);
       myPlot.Draw();
       c1->GetFrame()->DrawClone();
-      c1->SaveAs(plotsDir+variables[z]+"MCOnly_"+TString(icut[j])+"_"+suffix+".png");
+      c1->SaveAs(plotsDir+variables[z]+"DataMc_"+TString(icut[j])+"_"+suffix+".png");
+      c1->SaveAs(plotsDir+variables[z]+"DataMc_"+TString(icut[j])+"_"+suffix+".root");
 
       TCanvas* c2 = new TCanvas(Form("test_%d_%d_log", z, j),
 				Form("test_%d_%d_log", z, j));
@@ -231,7 +316,8 @@ void makeDataMcPlots(float lumi, bool blindData=false, int signalFactor=1)
       c2->SetLogy(1);
       myPlot.Draw();
       c2->GetFrame()->DrawClone();
-      c2->SaveAs(plotsDir+variables[z]+"MCOnly_"+TString(icut[j])+"_"+suffix+"_log.png");
+      c2->SaveAs(plotsDir+variables[z]+"DataMc_"+TString(icut[j])+"_"+suffix+"_log.png");
+      c2->SaveAs(plotsDir+variables[z]+"DataMc_"+TString(icut[j])+"_"+suffix+"_log.root");
     }
   }
   
