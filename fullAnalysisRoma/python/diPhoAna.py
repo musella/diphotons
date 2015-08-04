2e2e6680511c63ef11382de591175e37b37229d5
Simone Pigazzi <simone.pigazzini@live.it>
2015-08-04 16:19:02 +0200

    update

--- fullAnalysisRoma/python/diPhoAna.py
+++ fullAnalysisRoma/python/diPhoAna.py
@@ -15,15 +15,18 @@ process.GlobalTag.globaltag = 'POSTLS170_V5::All'
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32( 1000 ) )
 
 process.source = cms.Source("PoolSource",
-                            fileNames=cms.untracked.vstring(
-
+                            fileNames=cms.untracked.vstring
+                            (
         # Phys14, GG+jets, AnV1 
         #"/store/group/phys_higgs/cmshgg/musella/flashgg/ExoPhys14ANv1/diphotonsPhys14AnV1/GGJets_M-500To1000_Pt-50_13TeV-sherpa/ExoPhys14ANv1-diphotonsPhys14AnV1-v0-Phys14DR-PU20bx25_PHYS14_25_V1-v1/150330_141301/0000/diphotonsMicroAOD_1.root"
 
         # Phys14, RS 3TeV, AnV1 
-        "/store/group/phys_higgs/cmshgg/musella/flashgg/ExoPhys14ANv1/diphotonsPhys14AnV1/RSGravToGG_kMpl-01_M-3000_Tune4C_13TeV-pythia8/ExoPhys14ANv1-diphotonsPhys14AnV1-v0-Phys14DR-PU30bx50_PHYS14_25_V1-v1/150330_141554/0000/diphotonsMicroAOD_2.root"
+                                # "/store/group/phys_higgs/cmshgg/musella/flashgg/ExoPhys14ANv1/diphotonsPhys14AnV1/RSGravToGG_kMpl-01_M-3000_Tune4C_13TeV-pythia8/ExoPhys14ANv1-diphotonsPhys14AnV1-v0-Phys14DR-PU30bx50_PHYS14_25_V1-v1/150330_141554/0000/diphotonsMicroAOD_2.root"
+
+        # Spring15                        
+        "/store/group/phys_higgs/cmshgg/musella/flashgg/EXOSpring15_v1/Spring15BetaV2/RSGravToGG_kMpl-001_M-500_TuneCUEP8M1_13TeV-pythia8/EXOSpring15_v1-Spring15BetaV2-v0-RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/150717_101901/0000/diphotonsMicroAOD_1.root"
         )
-                            )
+)
 
 process.load("flashgg/MicroAOD/flashggPhotons_cfi")
 process.load("flashgg/MicroAOD/flashggDiPhotons_cfi")
@@ -40,7 +43,7 @@ process.diPhoAna = cms.EDAnalyzer('DiPhoAnalyzer',
                                   generatorInfo = cms.InputTag("generator"),
                                   dopureweight = cms.untracked.int32(0),
                                   sampleIndex  = cms.untracked.int32(101),   # 5
-                                  puWFileName  = cms.string('xxx'),   # chiara  
+                                  puWFileName  = cms.string(""),   # chiara  
                                   xsec         = cms.untracked.double(0.172872),
                                   kfac         = cms.untracked.double(1.),
                                   #sumDataset   = cms.untracked.double(2236.0)
