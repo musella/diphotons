5783fee17e89801110fd5b90056daa337508e4ad
crovelli <crovelli@gmail.com>
2015-09-22 16:55:21 +0200

    more infos added

--- fullAnalysisRoma/macro/tnpTreeFormatting.cc
+++ fullAnalysisRoma/macro/tnpTreeFormatting.cc
@@ -6,17 +6,19 @@
 
 using namespace std;
 
-void tnpTreeFormat(const char* filename) {
+void tnpTreeFormat(const char* filename, float lumiForW) {
 
   cout << "Formatting " << filename << endl;  
 
   TFile *fileOrig = 0;
   TTree *treeOrig = 0;
-
+  TH1F  *h_sumW = 0;
+  
   fileOrig = TFile::Open(filename);
   if( fileOrig ) {
     fileOrig->cd();
     treeOrig = (TTree*)fileOrig->Get("tnpAna/TaPtree");
+    h_sumW = (TH1F*)fileOrig->Get("tnpAna/h_sumW");
   } else {
     cout << "File " << filename << " not existing !" << endl;
     return;
@@ -38,7 +40,10 @@ void tnpTreeFormat(const char* filename) {
   
   std::vector<TTree*> trees; 
   trees.push_back(treeNew);
-  
+
+  // sum of weights in the dataset we ran on originally   
+  float sampleSumWeight = (float)h_sumW->Integral();   
+
   // original tree leaves
   Int_t           run   = 0;
   Int_t           event = 0;
@@ -46,6 +51,9 @@ void tnpTreeFormat(const char* filename) {
   Int_t           nvtx  = 0;
   Float_t         pu_weight = 0;
   Float_t         perEveW   = 0;
+  Float_t         totXsec = 0;
+  vector<float>   *electron_eta = 0;
+  vector<float>   *electron_pt = 0;
   vector<bool>    *isTagMediumEle    = 0;
   vector<bool>    *isTagTightEle     = 0;
   vector<bool>    *electron_matchHLT = 0;
@@ -53,8 +61,7 @@ void tnpTreeFormat(const char* filename) {
   vector<float>   *gamma_eta = 0;
   vector<int>     *gamma_presel   = 0;
   vector<int>     *gamma_fullsel  = 0;
-  vector<bool>    *gamma_matchHLT = 0;
-  vector<double>  *invMass    = 0;
+  vector<float>   *invMass    = 0;
   vector<int>     *eleIndex   = 0;
   vector<int>     *gammaIndex = 0;
 
@@ -65,6 +72,9 @@ void tnpTreeFormat(const char* filename) {
   TBranch        *b_nvtx;
   TBranch        *b_pu_weight;
   TBranch        *b_perEveW;
+  TBranch        *b_totXsec;
+  TBranch        *b_electron_eta;   //!   
+  TBranch        *b_electron_pt;   //!   
   TBranch        *b_isTagMediumEle;  
   TBranch        *b_isTagTightEle;
   TBranch        *b_electron_matchHLT; 
@@ -72,7 +82,6 @@ void tnpTreeFormat(const char* filename) {
   TBranch        *b_gamma_eta;   //!   
   TBranch        *b_gamma_presel; 
   TBranch        *b_gamma_fullsel; 
-  TBranch        *b_gamma_matchHLT;
   TBranch        *b_invMass; 
   TBranch        *b_eleIndex;  
   TBranch        *b_gammaIndex;
@@ -84,6 +93,9 @@ void tnpTreeFormat(const char* filename) {
   treeOrig->SetBranchAddress("nvtx", &nvtx, &b_nvtx);
   treeOrig->SetBranchAddress("pu_weight", &pu_weight, &b_pu_weight);
   treeOrig->SetBranchAddress("perEveW", &perEveW, &b_perEveW);
+  treeOrig->SetBranchAddress("totXsec", &totXsec, &b_totXsec);
+  treeOrig->SetBranchAddress("electron_eta", &electron_eta, &b_electron_eta);
+  treeOrig->SetBranchAddress("electron_pt", &electron_pt, &b_electron_pt);
   treeOrig->SetBranchAddress("isTagMediumEle", &isTagMediumEle, &b_isTagMediumEle);
   treeOrig->SetBranchAddress("isTagTightEle", &isTagTightEle, &b_isTagTightEle);
   treeOrig->SetBranchAddress("electron_matchHLT", &electron_matchHLT, &b_electron_matchHLT);
@@ -91,15 +103,16 @@ void tnpTreeFormat(const char* filename) {
   treeOrig->SetBranchAddress("gamma_eta", &gamma_eta, &b_gamma_eta);
   treeOrig->SetBranchAddress("gamma_presel", &gamma_presel, &b_gamma_presel);
   treeOrig->SetBranchAddress("gamma_fullsel", &gamma_fullsel, &b_gamma_fullsel);
-  treeOrig->SetBranchAddress("gamma_matchHLT", &gamma_matchHLT, &b_gamma_matchHLT);
   treeOrig->SetBranchAddress("invMass", &invMass, &b_invMass);
   treeOrig->SetBranchAddress("eleIndex", &eleIndex, &b_eleIndex);
   treeOrig->SetBranchAddress("gammaIndex", &gammaIndex, &b_gammaIndex);
 
   // New variables
+  float tag_pt, tag_absEta;
   float probe_pt, probe_absEta;
   int probe_fullsel;
   float mass;
+  float xsecWeight, weight;
   
   for(int i=0; i<(int)trees.size();i++) {
     TTree *theTreeNew = trees[i];
@@ -110,19 +123,24 @@ void tnpTreeFormat(const char* filename) {
     theTreeNew->Branch("lumi", &lumi, "lumi/I");
     theTreeNew->Branch("nvtx", &nvtx, "nvtx/I");
     theTreeNew->Branch("pu_weight", &pu_weight, "pu_weight/F");
-    theTreeNew->Branch("perEveW", &perEveW, "perEveW/F");
+    theTreeNew->Branch("tag_pt",&tag_pt,"tag_pt/F");
+    theTreeNew->Branch("tag_absEta",&tag_absEta,"tag_absEta/F");
     theTreeNew->Branch("probe_pt",&probe_pt,"probe_pt/F");
     theTreeNew->Branch("probe_absEta",&probe_absEta,"probe_absEta/F");
     theTreeNew->Branch("probe_fullsel", &probe_fullsel, "probe_fullsel/I");
     theTreeNew->Branch("mass", &mass, "mass/F");
+    theTreeNew->Branch("xsecWeight", &xsecWeight, "xsecWeight/F");
+    theTreeNew->Branch("weight", &weight, "weight/F");
   }
+
+  float xsecToWeight = 3.*2008.4;   // chiara! fixme in the job submission
 
   for(int i=0; i<nentriesOrig; i++) {
     
     if (i%10000 == 0) std::cout << ">>> Event # " << i << " / " << nentriesOrig << " entries" << std::endl; 
     treeOrig->GetEntry(i);
 
-    for (int ii=0; ii<invMass->size(); ii++) {
+    for (unsigned int ii=0; ii<invMass->size(); ii++) {
       
       mass = (float)(invMass->at(ii));
       if (mass<60 || mass>120) continue;
@@ -133,25 +151,37 @@ void tnpTreeFormat(const char* filename) {
       
       // further selection on probe
       if (!gamma_presel->at(gammaIndex->at(ii)))   continue;
-      //if (!gamma_matchHLT->at(gammaIndex->at(ii))) continue;
       
       // now making flat tree
+      tag_absEta = fabs(electron_eta->at(eleIndex->at(ii)));
+      tag_pt = electron_pt->at(eleIndex->at(ii));
       probe_pt = gamma_pt->at(gammaIndex->at(ii));
       probe_absEta  = fabs(gamma_eta->at(gammaIndex->at(ii)));
       probe_fullsel = gamma_fullsel->at(gammaIndex->at(ii));  
 
+      // weights
+      if (run==1) {   // MC                                                                                                                   
+	xsecWeight = perEveW * lumiForW * xsecToWeight / sampleSumWeight;
+	weight     = xsecWeight * pu_weight;
+      } else {
+	xsecWeight = 1.;
+	weight     = 1.;
+      }
+
       treeNew->Fill();
-      treeNew->GetEntries();
+      // treeNew->GetEntries();
     }
   }
 
 
   // new info
+  fileNew->ls();
   fileNew->cd();
   TDirectory *myDir = (TDirectory*)fileNew->mkdir("tnpAna");
   myDir->cd();
   treeNew->Write();
   fileNew->Close();
+  fileNew->ls();
 
   fileOrig->cd();
   fileOrig->Close();  
