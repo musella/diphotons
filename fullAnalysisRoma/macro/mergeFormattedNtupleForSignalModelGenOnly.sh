bb90dc9231e720f7f5b8dcc3f29a9fb9b07f81e2
crovelli <crovelli@gmail.com>
2015-10-28 14:31:52 +0100

    first commit: from common format to our format for signal model, gen only

---
+++ fullAnalysisRoma/macro/mergeFormattedNtupleForSignalModelGenOnly.sh
@@ -0,0 +1,77 @@
+#! /bin/sh                                                                                                                   
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M500.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M500_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M500_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M500_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M500_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M625.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M625_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M625_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M625_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M625_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M750.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M750_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M750_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M750_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M750_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M875.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M875_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M875_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M875_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M875_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M1000.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M1000_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M1000_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M1000_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M1000_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M1125.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M1125_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M1125_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M1125_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M1125_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M1250.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M1250_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M1250_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M1250_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M1250_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M1375.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M1375_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M1375_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M1375_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M1375_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M1500.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M1500_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M1500_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M1500_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M1500_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M1625.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M1625_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M1625_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M1625_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M1625_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M1750.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M1750_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M1750_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M1750_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M1750_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M1875.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M1875_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M1875_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M1875_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M1875_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M2000.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M2000_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M2000_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M2000_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M2000_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M2125.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M2125_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M2125_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M2125_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M2125_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M2250.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M2250_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M2250_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M2250_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M2250_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M2375.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M2375_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M2375_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M2375_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M2375_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M2500.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M2500_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M2500_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M2500_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M2500_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M2625.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M2625_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M2625_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M2625_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M2625_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M2750.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M2750_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M2750_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M2750_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M2750_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M2875.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M2875_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M2875_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M2875_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M2875_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M3000.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M3000_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M3000_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M3000_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M3000_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M3125.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M3125_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M3125_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M3125_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M3125_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M3250.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M3250_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M3250_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M3250_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M3250_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M3375.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M3375_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M3375_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M3375_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M3375_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M3500.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M3500_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M3500_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M3500_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M3500_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M3625.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M3625_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M3625_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M3625_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M3625_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M3750.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M3750_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M3750_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M3750_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M3750_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M3875.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M3875_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M3875_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M3875_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M3875_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M4000.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M4000_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M4000_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M4000_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M4000_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M4125.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M4125_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M4125_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M4125_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M4125_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M4250.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M4250_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M4250_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M4250_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M4250_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M4375.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M4375_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M4375_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M4375_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M4375_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M4500.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M4500_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M4500_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M4500_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M4500_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M4625.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M4625_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M4625_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M4625_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M4625_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M4750.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M4750_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M4750_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M4750_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M4750_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M4875.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M4875_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M4875_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M4875_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M4875_catEE.root 
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M5000.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M5000_catEB.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M5000_catEB.root /tmp/crovelli/FormSigMod_genGenIso_kpl001_M5000_catEE.root /tmp/crovelli/FormSigMod_genNonGenIso_kpl001_M5000_catEE.root 
+
+rm /tmp/crovelli/FormSigMod_*GenIso_kpl*_M*_cat*.root
