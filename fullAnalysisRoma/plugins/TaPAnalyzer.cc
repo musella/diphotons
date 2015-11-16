fc3ccec4d8952a3559b80974c8c1619b1d5513f4
crovelli <crovelli@gmail.com>
2015-11-16 16:34:58 +0100

    moving to true pileup

--- fullAnalysisRoma/plugins/TaPAnalyzer.cc
+++ fullAnalysisRoma/plugins/TaPAnalyzer.cc
@@ -85,8 +85,6 @@ private:
   edm::EDGetTokenT<pat::TriggerObjectStandAloneCollection> triggerObjects_;  
   EDGetTokenT<View<pat::MET> > MetToken_;
   EDGetTokenT<View<reco::GenParticle> > genPartToken_;
-  edm::EDGetTokenT<EcalRecHitCollection> ecalHitEBToken_;
-  edm::EDGetTokenT<EcalRecHitCollection> ecalHitEEToken_;
 
   // sample-dependent parameters needed for the analysis
   int dopureweight_;
@@ -170,13 +168,11 @@ TaPAnalyzer::TaPAnalyzer(const edm::ParameterSet& iConfig):
   vertexToken_(consumes<View<reco::Vertex> >(iConfig.getUntrackedParameter<InputTag> ("VertexTag", InputTag("offlineSlimmedPrimaryVertices")))),
   electronToken_( consumes<View<flashgg::Electron> >( iConfig.getParameter<InputTag> ("ElectronTag"))),
   photonToken_(consumes<View<flashgg::Photon> >(iConfig.getUntrackedParameter<InputTag> ("PhotonTag", InputTag("flashggPhotons")))),
-  PileUpToken_(consumes<View<PileupSummaryInfo> >(iConfig.getUntrackedParameter<InputTag> ("PileUpTag", InputTag("addPileupInfo")))),
+  PileUpToken_(consumes<View<PileupSummaryInfo> >(iConfig.getUntrackedParameter<InputTag> ("PileUpTag"))), 
   triggerBits_(consumes<edm::TriggerResults>(iConfig.getParameter<edm::InputTag>("bits"))),
   triggerObjects_(consumes<pat::TriggerObjectStandAloneCollection>(iConfig.getParameter<edm::InputTag>("objects"))),
   MetToken_( consumes<View<pat::MET> >(iConfig.getParameter<InputTag>("MetTag" ))),
-  genPartToken_(consumes<View<reco::GenParticle> >(iConfig.getUntrackedParameter<InputTag> ("GenParticlesTag", InputTag("flashggPrunedGenParticles")))),
-  ecalHitEBToken_(consumes<EcalRecHitCollection>(iConfig.getParameter<edm::InputTag>("reducedBarrelRecHitCollection"))),
-  ecalHitEEToken_(consumes<EcalRecHitCollection>(iConfig.getParameter<edm::InputTag>("reducedEndcapRecHitCollection")))
+  genPartToken_(consumes<View<reco::GenParticle> >(iConfig.getUntrackedParameter<InputTag> ("GenParticlesTag", InputTag("flashggPrunedGenParticles"))))
 { 
   dopureweight_ = iConfig.getUntrackedParameter<int>("dopureweight", 0);
   sampleIndex_  = iConfig.getUntrackedParameter<int>("sampleIndex",0);
@@ -229,16 +225,12 @@ void TaPAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
   edm::Handle<View<reco::GenParticle> > genParticles;
   if (sampleID>0 && sampleID<10000) iEvent.getByToken( genPartToken_, genParticles );
 
-  Handle< EcalRecHitCollection > EcalBarrelRecHits;
-  iEvent.getByToken(ecalHitEBToken_, EcalBarrelRecHits);
-  Handle< EcalRecHitCollection > EcalEndcapRecHits;
-  iEvent.getByToken(ecalHitEEToken_, EcalEndcapRecHits);
-
   Handle<View<pat::MET> > METs;
   iEvent.getByToken( MetToken_, METs );
   if( METs->size() != 1 ) std::cout << "WARNING number of MET is not equal to 1" << std::endl; 
   Ptr<pat::MET> theMET = METs->ptrAt( 0 );
   t1pfmet = theMET->pt();
+  
 
   // --------------------------------------------------
   // Event info
@@ -262,13 +254,13 @@ void TaPAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
       {
 	Int_t pu_bunchcrossing = PileupInfos->ptrAt( PVI )->getBunchCrossing();
 	if( pu_bunchcrossing == 0 ) {
-	  pu_n = PileupInfos->ptrAt( PVI )->getPU_NumInteractions();
+	  pu_n = PileupInfos->ptrAt( PVI )->getTrueNumInteractions();         
 	}
       }
     if (dopureweight_) 
       pu_weight = GetPUWeight(pu_n);         
   }
-  
+
   // x-sec * kFact for MC only 
   totXsec = 1.;
   if (sampleID>0 && sampleID<10000) totXsec = xsec_ * kfac_;
@@ -316,6 +308,7 @@ void TaPAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
   if (sampleID>=10000) theTnPPath = theTnPPathData;
   else theTnPPath = theTnPPathMc;
   
+
   // check if the event fired the TnP path
   bool fired = false;
   for (unsigned int i = 0, n = triggerBits->size(); i < n; ++i) {
@@ -565,20 +558,7 @@ void TaPAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
 
 	  // saturation 
 	  bool isKsaturated = false;
-	  if (passPresel) {
-	    DetId seedDetId = ( (g1->superCluster())->seed() )->seed();
-	    if(seedDetId) {
-	      if(seedDetId.subdetId()==EcalEndcap) {
-		EcalRecHitCollection::const_iterator itseed = EcalEndcapRecHits->find( seedDetId );
-		if (itseed != EcalEndcapRecHits->end())
-		  isKsaturated = itseed->checkFlag(EcalRecHit::kSaturated);
-	      } else {
-		EcalRecHitCollection::const_iterator itseed = EcalBarrelRecHits->find( seedDetId );
-		if (itseed != EcalBarrelRecHits->end())
-		  isKsaturated = itseed->checkFlag(EcalRecHit::kSaturated);     
-	      }
-	    }
-	  }
+	  // isKsaturated = g1->checkStatusFlag('kSaturated');   // chiaraaaaaaa
 
 	  // full selection
 	  bool passFullSelel = isGammaSelected( rho, pt, scEta, R9noZS, chIso, neuIso, phoIso, HoE, sieienoZS, eleVeto, isKsaturated); 
