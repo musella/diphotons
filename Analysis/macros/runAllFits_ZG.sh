deb13ee01fc70be9649e251f9e813dfe5940f67b
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2016-01-19 10:34:20 +0100

    works on mc

--- Analysis/macros/runAllFits_ZG.sh
+++ Analysis/macros/runAllFits_ZG.sh
@@ -1,11 +1,19 @@
 ## parallel --ungroup -j 1 './combine_maker.sh full_analysis_spring15_7415v2_sync_v6 --data-file ~/public/workspace/exo/full_analysis_spring15_7415v2_sync_v5_data_ecorr/output.root --lumi 2.4 --fit-name cic2 --plot-fit-bands --rescale-signal-to 1e-3  --parametric-signal ~/eos/cms/store/user/crovelli/WSdiphotonConSmearings/nominalWSwithSmear_k{}_m1000to4900.root --parametric-signal ~/eos/cms/store/user/crovelli/WSdiphotonConSmearings/nominalWSwithSmear_k{}_m500to998.root  --parametric-signal-xsection xsections.json --parametric-signal-acceptance  acceptance_pu.json --load lumi.json  --compute-fwhm --generate-ws-bkgnbias --only-coups {} --label approval --minos-bands' ::: 001 005 007 01 015 02
 
-./combine_maker_ZG.sh EventYields_v0 --lumi 2.3 \
-    --fit-name fit_v0 --plot-fit-bands  --minos-bands \
+./combine_maker_ZG.sh EventYields_v0 --luminosity 2.3 \
+    --fit-name fit_v0 \
+    --mc-data \
     --fit-background \
     --label combination_pcorr --load combine_maker_ZG.json \
-    --redo-input
+    --redo-input \
+    --generate-signal-dataset \
+    --gaussian-signal \
+    --generate-datacard \
+    --no-use-custom-pdfs \
+    --signal-scalefactor-forpdf 1
 
+    #--minos-bands \
+    #--plot-fit-bands  \
 ### parallel --ungroup './splitallcards.sh full_analysis_spring15_7415v2_sync_v5_data_ecorr_cic2_default_shapes_approval_lumi_2.4 {}' ::: EBEB EBEE
 
 ## ./combineall.sh input_8TeV_cic1_default_shapes_combination_pcorr_lumi_19.6 001 -M ProfileLikelihood --pvalue --significance  --hadd  &
