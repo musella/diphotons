ec3c1c3c2fddafaa931fc18ffba05b927ec3de9a
crovelli <crovelli@gmail.com>
2016-01-25 14:30:09 +0100

    rereco numbers added; moved to sc eta

--- fullAnalysisRoma/macro/signalModelFormatting.cc
+++ fullAnalysisRoma/macro/signalModelFormatting.cc
@@ -5,12 +5,11 @@
 #include <TRandom.h>
 
 // To be modified
-static const Int_t nominal     = 1;
-static const Int_t smearDown   = 0;
-static const Int_t smearUp     = 0;
-static const Int_t scaleDown   = 0;
+static const Int_t prompt      = 0;
+static const Int_t guessRereco = 0;
+static const Int_t rereco      = 1;
 static const Int_t scaleUp     = 0;
-static const Int_t checkRereco = 0;
+static const Int_t scaleDown   = 0;
 
 using namespace std;
 
@@ -20,12 +19,13 @@ void signalModelFormat(const char* filename, TString kinGenIso, TString kpl, TSt
   TRandom myRandom(12345);
 
   cout << "Formatting " << filename << " from Pasquale's to our format for signal model studies" << endl;  
-  if (nominal)     cout << "nominal smearings applied"   << endl;
-  if (smearUp)     cout << "smearUp smearings applied"   << endl;
-  if (smearDown)   cout << "smearDown smearings applied" << endl;
-  if (scaleUp)     cout << "scaleUp applied"   << endl;
-  if (scaleDown)   cout << "scaleDown applied" << endl;
-  if (checkRereco) cout << "[potential] rereco smearings applied"   << endl;
+  if (prompt)      cout << "prompt reco smearings applied"    << endl;
+  if (guessRereco) cout << "guessed rereco smearings applied" << endl;
+  if (rereco)      cout << "74x rereco smearings by Giuseppe applied" << endl;
+  if (!prompt && !guessRereco && !rereco) { cout << "choose some smearings! " << endl; }
+  if (scaleUp)     cout << "scale 1% up"   << endl;
+  if (scaleDown)   cout << "scale 1% down" << endl;
+  if (!scaleDown && !scaleUp) cout << "nominal scale" << endl;
 
   TFile *fileOrig = 0;
   TTree *treeOrig = 0;
@@ -62,31 +62,31 @@ void signalModelFormat(const char* filename, TString kinGenIso, TString kpl, TSt
   trees.push_back(treeNew);
 
   // original tree leaves
-  Float_t puweight   = 0.;
-  Float_t mass       = 0.;
-  Float_t genMass    = 0.;
-  Float_t leadEta    = 0.;
-  Float_t subleadEta = 0.;
-  Float_t leadR9     = 0.;
-  Float_t subleadR9  = 0.;
+  Float_t puweight     = 0.;
+  Float_t mass         = 0.;
+  Float_t genMass      = 0.;
+  Float_t leadR9       = 0.;
+  Float_t subleadR9    = 0.;
+  Float_t leadScEta    = 0.;
+  Float_t subleadScEta = 0.;
   
   // List of branches - original tree
   TBranch  *b_puweight;
   TBranch  *b_mass;
   TBranch  *b_genMass;
-  TBranch  *b_leadEta;
-  TBranch  *b_subleadEta;
   TBranch  *b_leadR9;
   TBranch  *b_subleadR9;
+  TBranch  *b_leadScEta;
+  TBranch  *b_subleadScEta;
 
   // Set branch addresses and branch pointers 
-  treeOrig->SetBranchAddress("puweight",   &puweight,   &b_puweight);
-  treeOrig->SetBranchAddress("mass",       &mass,       &b_mass);
-  treeOrig->SetBranchAddress("genMass",    &genMass,    &b_genMass);
-  treeOrig->SetBranchAddress("leadEta",    &leadEta,    &b_leadEta);
-  treeOrig->SetBranchAddress("subleadEta", &subleadEta, &b_subleadEta);
-  treeOrig->SetBranchAddress("leadR9",     &leadR9,     &b_leadR9);
-  treeOrig->SetBranchAddress("subleadR9",  &subleadR9,  &b_subleadR9);
+  treeOrig->SetBranchAddress("puweight",     &puweight,     &b_puweight);
+  treeOrig->SetBranchAddress("mass",         &mass,         &b_mass);
+  treeOrig->SetBranchAddress("genMass",      &genMass,      &b_genMass);
+  treeOrig->SetBranchAddress("leadR9",       &leadR9,       &b_leadR9);
+  treeOrig->SetBranchAddress("subleadR9",    &subleadR9,    &b_subleadR9);
+  treeOrig->SetBranchAddress("leadScEta",    &leadScEta,    &b_leadScEta);
+  treeOrig->SetBranchAddress("subleadScEta", &subleadScEta, &b_subleadScEta);
 
   // New variables
   float mgg, unsmearedMgg, mggGen;
@@ -109,70 +109,102 @@ void signalModelFormat(const char* filename, TString kinGenIso, TString kpl, TSt
     
     treeOrig->GetEntry(i);
 
-    if (fabs(leadEta)>1.5 && fabs(subleadEta)>1.5) continue;
+    // EE-EE excluded
+    if (fabs(leadScEta)>1.5 && fabs(subleadScEta)>1.5) continue;
 
+    // 2 classes
     eventClass = -1;
-    if (fabs(leadEta)<1.5 && fabs(subleadEta)<1.5) eventClass = 0;
+    if (fabs(leadScEta)<1.5 && fabs(subleadScEta)<1.5) eventClass = 0;
     else eventClass = 1;
 
+    // R9 classes
     if (leadR9>0.94 && subleadR9>0.94) highR9=1;
     else highR9=0;
 
+    // before smearings
     unsmearedMgg = mass;
+
+    // gen level
     mggGen = genMass;
 
+    // hardcoded!
+    float smearEBlowEtaHighR9  = 0.;
+    float smearEBlowEtaLowR9   = 0.;
+    float smearEBhighEtaHighR9 = 0.;
+    float smearEBhighEtaLowR9  = 0.;
+    float smearEElowEtaHighR9  = 0.;
+    float smearEElowEtaLowR9   = 0.;
+    float smearEEhighEtaHighR9 = 0.;
+    float smearEEhighEtaLowR9  = 0.;
 
-    // Nominal or systematics
-    // hardcoded! - init    
-    float smearEBlowEta     = 0.013898;
-    float smearEBhighEta    = 0.0189895;
-    float smearEElowEta     = 0.027686;
-    float smearEEhighEta    = 0.031312;
-    float smearEBlowEtaErr  = 0.000153567;
-    float smearEBhighEtaErr = 0.000518756;
-    float smearEElowEtaErr  = 0.000445363;
-    float smearEEhighEtaErr = 0.000372622;
-    // hardcoded! - end 
+    // prompt reco smearings
+    if (prompt) {
+      smearEBlowEtaHighR9  = 0.014142;
+      smearEBlowEtaLowR9   = 0.013654;
+      smearEBhighEtaHighR9 = 0.017120;
+      smearEBhighEtaLowR9  = 0.020859;
+      smearEElowEtaHighR9  = 0.027289;
+      smearEElowEtaLowR9   = 0.028083;
+      smearEEhighEtaHighR9 = 0.030831;
+      smearEEhighEtaLowR9  = 0.031793;
+    }
+
+    // guess for re-reco   
+    if (guessRereco) {   
+      smearEBlowEtaHighR9  = 0.009;
+      smearEBlowEtaLowR9   = 0.009;
+      smearEBhighEtaHighR9 = 0.015;
+      smearEBhighEtaLowR9  = 0.015;
+      smearEElowEtaHighR9  = 0.021;
+      smearEElowEtaLowR9   = 0.021;
+      smearEEhighEtaHighR9 = 0.021;
+      smearEEhighEtaLowR9  = 0.021;
+    } 
+
+    // preliminary by Giuseppe (dec15), photon-trtained regression, 74x rereco      
+    if (rereco) { 
+      smearEBlowEtaHighR9  = 0.0087;
+      smearEBlowEtaLowR9   = 0.0105;
+      smearEBhighEtaHighR9 = 0.0130;
+      smearEBhighEtaLowR9  = 0.0181;
+      smearEElowEtaHighR9  = 0.0204;
+      smearEElowEtaLowR9   = 0.0223;
+      smearEEhighEtaHighR9 = 0.0231;
+      smearEEhighEtaLowR9  = 0.0260;
+    }
+
     float theGaussMean = 1.;
-    if (smearUp) {
-      smearEBlowEta  = smearEBlowEta  + smearEBlowEtaErr;
-      smearEBhighEta = smearEBhighEta + smearEBhighEtaErr;
-      smearEElowEta  = smearEElowEta  + smearEElowEtaErr;
-      smearEEhighEta = smearEEhighEta + smearEEhighEtaErr;
-    } else if (smearDown) {
-      smearEBlowEta  = smearEBlowEta  - smearEBlowEtaErr;
-      smearEBhighEta = smearEBhighEta - smearEBhighEtaErr;
-      smearEElowEta  = smearEElowEta  - smearEElowEtaErr;
-      smearEEhighEta = smearEEhighEta - smearEEhighEtaErr;
-    } else if (scaleUp) {
+    if (scaleUp) {
       theGaussMean = 1.01;
     } else if (scaleDown) {
       theGaussMean = 0.99;
-    } else if (checkRereco) {
-      smearEBlowEta  = 0.009;
-      smearEBhighEta = 0.015;
-      smearEElowEta  = 0.021;
-      smearEEhighEta = 0.021;
     }
+
 
     // Apply smearings
     float theFirstSmear = 0.;   
-    if (fabs(leadEta)<1) theFirstSmear = smearEBlowEta;
-    else if (fabs(leadEta)>=1 && fabs(leadEta)<1.5)  theFirstSmear = smearEBhighEta;
-    else if (fabs(leadEta)>=1.5 && fabs(leadEta)<2.) theFirstSmear = smearEElowEta;
-    else if (fabs(leadEta)>=2 && fabs(leadEta)<2.5)  theFirstSmear = smearEEhighEta;
+    if (fabs(leadScEta)<1 && leadR9>=0.94)      theFirstSmear = smearEBlowEtaHighR9;
+    else if (fabs(leadScEta)<1 && leadR9<0.94)  theFirstSmear = smearEBlowEtaLowR9;
+    else if (fabs(leadScEta)>=1 && fabs(leadScEta)<1.5 && leadR9>=0.94)  theFirstSmear = smearEBhighEtaHighR9;
+    else if (fabs(leadScEta)>=1 && fabs(leadScEta)<1.5 && leadR9<0.94)   theFirstSmear = smearEBhighEtaLowR9;
+    else if (fabs(leadScEta)>=1.5 && fabs(leadScEta)<2. && leadR9>=0.94) theFirstSmear = smearEElowEtaHighR9;
+    else if (fabs(leadScEta)>=1.5 && fabs(leadScEta)<2. && leadR9<0.94)  theFirstSmear = smearEElowEtaLowR9;
+    else if (fabs(leadScEta)>=2 && fabs(leadScEta)<2.5 && leadR9>=0.94)  theFirstSmear = smearEEhighEtaHighR9;
+    else if (fabs(leadScEta)>=2 && fabs(leadScEta)<2.5 && leadR9<0.94)   theFirstSmear = smearEEhighEtaLowR9;
     //
     float theSecondSmear = 0.;   
-    if (fabs(subleadEta)<1) theSecondSmear = smearEBlowEta;
-    else if (fabs(subleadEta)>=1 && fabs(subleadEta)<1.5)  theSecondSmear = smearEBhighEta;
-    else if (fabs(subleadEta)>=1.5 && fabs(subleadEta)<2.) theSecondSmear = smearEElowEta;
-    else if (fabs(subleadEta)>=2 && fabs(subleadEta)<2.5)  theSecondSmear = smearEEhighEta;
+    if (fabs(subleadScEta)<1 && subleadR9>=0.94)      theSecondSmear = smearEBlowEtaHighR9;
+    else if (fabs(subleadScEta)<1 && subleadR9<0.94)  theSecondSmear = smearEBlowEtaLowR9;
+    else if (fabs(subleadScEta)>=1 && fabs(subleadScEta)<1.5 && subleadR9>=0.94)  theSecondSmear = smearEBhighEtaHighR9;
+    else if (fabs(subleadScEta)>=1 && fabs(subleadScEta)<1.5 && subleadR9<0.94)   theSecondSmear = smearEBhighEtaLowR9;
+    else if (fabs(subleadScEta)>=1.5 && fabs(subleadScEta)<2. && subleadR9>=0.94) theSecondSmear = smearEElowEtaHighR9;
+    else if (fabs(subleadScEta)>=1.5 && fabs(subleadScEta)<2. && subleadR9<0.94)  theSecondSmear = smearEElowEtaLowR9;
+    else if (fabs(subleadScEta)>=2 && fabs(subleadScEta)<2.5 && subleadR9>=0.94)  theSecondSmear = smearEEhighEtaHighR9;
+    else if (fabs(subleadScEta)>=2 && fabs(subleadScEta)<2.5 && subleadR9<0.94)   theSecondSmear = smearEEhighEtaLowR9;
     //
     float theGaussSigma = 0.5 * sqrt (theFirstSmear*theFirstSmear + theSecondSmear*theSecondSmear);
     float fromGauss = myRandom.Gaus(theGaussMean,theGaussSigma);
     mgg = mass*fromGauss;
-
-    ////mgg = mass;
 
     treeNew->Fill();
   }
