5541ef9883e249e3c40e77d259fa5c7f0dd1b4dc
Simone Pigazzi <simone.pigazzini@live.it>
2016-02-08 12:09:06 +0100

    import diphotons0T_v2 catalog + vtx studies analyzer

--- Analysis/plugins/ZeroTeslaVtxAnalyzer.cc
+++ Analysis/plugins/ZeroTeslaVtxAnalyzer.cc
@@ -2,6 +2,7 @@
 #include <map>
 
 #include "TTree.h"
+#include "TRandom.h"
 
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/Event.h"
@@ -36,12 +37,17 @@ namespace diphotons
 
     private:
         TTree* tree_;
-        float gen_z;
+        float gen_z=0;
+        float m=0;
+        float m_sm=0;
+        int best_trk=0;
+        int best_trk_away=0;
+        vector<int> original_id;
         vector<float> reco_z;
-        vector<int> cone_dipho;
-        vector<int> cone_middle;
-        vector<int> cone_back;
-        vector<int> trk_sum;
+        vector<float> sum_et2;
+        vector<int>   cone_dipho;
+        vector<int>   cone_middle;
+        vector<int>   cone_back;
         Service<TFileService> fs_;
         EDGetTokenT<View<flashgg::DiPhotonCandidate> > diphoToken_;
     };
@@ -51,20 +57,31 @@ namespace diphotons
     {
         tree_ = fs_->make<TTree>("vtxs_tree", "vtxs");
         tree_->Branch("gen_z", &gen_z, "gen_z/F");
+        tree_->Branch("m", &m, "m/F");
+        tree_->Branch("m_sm", &m_sm, "m_sm/F");
+        tree_->Branch("best_trk", &best_trk, "best_trk/I");
+        tree_->Branch("best_trk_away", &best_trk_away, "best_trk_away/I");
+        tree_->Branch("original_id", "std::vector<int>", &original_id);
         tree_->Branch("reco_z", "std::vector<float>", &reco_z);
+        tree_->Branch("sum_et2", "std::vector<float>", &sum_et2);
         tree_->Branch("cone_dipho", "std::vector<int>", &cone_dipho);
         tree_->Branch("cone_middle", "std::vector<int>", &cone_middle);
         tree_->Branch("cone_back", "std::vector<int>", &cone_back);
-        tree_->Branch("trk_sum", "std::vector<int>", &trk_sum);
     }
 
     void ZeroTeslaVtxAnalyzer::analyze(const Event& event, const EventSetup& setup)
     {
+        int most_trk=0;
+        int most_trk_away=0;
+        best_trk=0;
+        best_trk_away=0;
+        original_id.clear();
         reco_z.clear();
+        sum_et2.clear();
         cone_dipho.clear();
         cone_middle.clear();
         cone_back.clear();
-        trk_sum.clear();
+        
         
         Handle<View<flashgg::DiPhotonCandidate> > diphoHandle;
         event.getByToken(diphoToken_, diphoHandle);
@@ -72,32 +89,70 @@ namespace diphotons
         if(diphoHandle->size() != 0)
         {
             flashgg::DiPhotonCandidate cand0 = diphoHandle->at(0);
-            for(auto& vtx : cand0.getVtxsCones())
-            {                
-                float vtx_z = vtx.second.first;
-                vector<int> cones = vtx.second.second;
-                int vtx_trk_sum = cones[0]+cones[1]+cones[2];
-                cout << cones[0] << "  " << cones[1] << "  " << cones[2] << endl;
-                for(unsigned int iVtx=0; iVtx<trk_sum.size(); ++iVtx)
+            m = cand0.mass();
+            gen_z = cand0.genPV().z();
+            int current_vtx=-1;
+
+            if(fabs(cand0.leadingPhoton()->eta()) < 1)
+                m_sm = m * sqrt(gRandom->Gaus(1, 0.008));
+            else if(fabs(cand0.leadingPhoton()->eta()) < 1.5)
+                m_sm = m * sqrt(gRandom->Gaus(1, 0.0115));
+            else if(fabs(cand0.leadingPhoton()->eta()) < 2)
+                m_sm = m * sqrt(gRandom->Gaus(1, 0.0201));
+            else
+                m_sm = m * sqrt(gRandom->Gaus(1, 0.023));
+
+            if(fabs(cand0.subLeadingPhoton()->eta()) < 1)
+                m_sm = m * sqrt(gRandom->Gaus(1, 0.008));
+            else if(fabs(cand0.subLeadingPhoton()->eta()) < 1.5)
+                m_sm = m * sqrt(gRandom->Gaus(1, 0.0115));
+            else if(fabs(cand0.subLeadingPhoton()->eta()) < 2)
+                m_sm = m * sqrt(gRandom->Gaus(1, 0.0201));
+            else
+                m_sm = m * sqrt(gRandom->Gaus(1, 0.023));
+            
+            for(auto& vtx : cand0.getVtxs())
+            {
+                ++current_vtx;
+                float vtx_z = vtx->z();
+                float vtx_sum_et2 = cand0.getVtxSumEt2(vtx);
+                vector<int> cones = cand0.getVtxCones(vtx);
+                int tmp_trk_away = cones[1] + cones[2];
+                int tmp_trk = cones[0] + tmp_trk_away;
+                int insert_pos=-1;
+                for(unsigned int iVtx=0; iVtx<sum_et2.size(); ++iVtx)
                 {
-                    if(vtx_trk_sum > trk_sum[iVtx])
+                    if(vtx_sum_et2 > sum_et2[iVtx])
                     {
-                        trk_sum.emplace(trk_sum.begin()+iVtx, vtx_trk_sum);
+                        original_id.emplace(original_id.begin()+iVtx, current_vtx);
+                        sum_et2.emplace(sum_et2.begin()+iVtx, vtx_sum_et2);
+                        reco_z.emplace(reco_z.begin()+iVtx, vtx_z);
                         cone_dipho.emplace(cone_dipho.begin()+iVtx, cones[0]);
                         cone_middle.emplace(cone_middle.begin()+iVtx, cones[1]);
                         cone_back.emplace(cone_back.begin()+iVtx, cones[2]);
-                        reco_z.emplace(reco_z.begin()+iVtx, vtx_z);
-                        vtx_trk_sum=-1;
+                        insert_pos=iVtx;
                         break;
                     }
                 }
-                if(vtx_trk_sum != -1)
+                if(insert_pos == -1)
                 {
-                    trk_sum.push_back(vtx_trk_sum);
+                    original_id.push_back(current_vtx);
+                    sum_et2.push_back(vtx_sum_et2);
+                    reco_z.push_back(vtx_z);
                     cone_dipho.push_back(cones[0]);
                     cone_middle.push_back(cones[1]);
                     cone_back.push_back(cones[2]);
-                    reco_z.push_back(vtx_z);
+                    insert_pos = sum_et2.size()-1;
+                }
+                if(tmp_trk > most_trk)
+                {
+                    most_trk = tmp_trk;
+                    best_trk = insert_pos;
+                }
+                if(tmp_trk_away > most_trk_away)
+                {
+                    most_trk_away = tmp_trk_away;
+                    best_trk_away = insert_pos;
                 }
             }
             tree_->Fill();
