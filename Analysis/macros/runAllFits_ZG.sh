2e209f4a6848ade85c7510e6f0db138f7ba03cae
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2016-02-16 12:56:08 +0100

    default is fit_em, doubleCB fits

--- Analysis/macros/runAllFits_ZG.sh
+++ Analysis/macros/runAllFits_ZG.sh
@@ -1,17 +1,23 @@
 ## parallel --ungroup -j 1 './combine_maker.sh full_analysis_spring15_7415v2_sync_v6 --data-file ~/public/workspace/exo/full_analysis_spring15_7415v2_sync_v5_data_ecorr/output.root --lumi 2.4 --fit-name cic2 --plot-fit-bands --rescale-signal-to 1e-3  --parametric-signal ~/eos/cms/store/user/crovelli/WSdiphotonConSmearings/nominalWSwithSmear_k{}_m1000to4900.root --parametric-signal ~/eos/cms/store/user/crovelli/WSdiphotonConSmearings/nominalWSwithSmear_k{}_m500to998.root  --parametric-signal-xsection xsections.json --parametric-signal-acceptance  acceptance_pu.json --load lumi.json  --compute-fwhm --generate-ws-bkgnbias --only-coups {} --label approval --minos-bands' ::: 001 005 007 01 015 02
 
-./combine_maker_ZG.sh EventYields_v0 --luminosity 2.3 \
-    --fit-name fit_v0 \
-    --mc-data \
+./combine_maker_ZG.sh EventYields_v0_silver --luminosity 2.6 \
+    --fit-name ${1:-fit_em} \
     --fit-background \
+    --mc-data \
     --label combination_pcorr --load combine_maker_ZG.json \
     --redo-input \
     --generate-signal-dataset \
-    --gaussian-signal \
+    --doubleCB-signal \
     --generate-datacard \
-    --no-use-custom-pdfs \
+    --saveas png,pdf,eps \
     --signal-scalefactor-forpdf 1
 
+#./combine_maker_ZG.sh EventYields_v0 --luminosity 2.3 \
+    #--gaussian-signal \
+    #--no-use-custom-pdfs \
+    #--minos-bands \
+    #--plot-fit-bands  \
+    #--no-use-custom-pdfs \
     #--minos-bands \
     #--plot-fit-bands  \
 
