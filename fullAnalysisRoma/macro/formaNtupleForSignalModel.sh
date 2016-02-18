3e7c84bcfefd7b92648b3eae9ed734ae8b0090e7
crovelli <crovelli@gmail.com>
2016-02-18 16:29:11 +0100

    using 001 where possible

--- fullAnalysisRoma/macro/formaNtupleForSignalModel.sh
+++ fullAnalysisRoma/macro/formaNtupleForSignalModel.sh
@@ -3,169 +3,84 @@
 root -l -b <<EOF
 .L signalModelFormatting.cc++
 
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","1000","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","1000","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","1000","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","1000","EELowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","1000","EBHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","1000","EBLowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","1000","EEHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","1000","EELowR9")
 
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","1250","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","1250","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","1250","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","1250","EELowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","1250","EBHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","1250","EBLowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","1250","EEHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","1250","EELowR9")
 
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","1500","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","1500","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","1500","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","1500","EELowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","1750","EBHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","1750","EBLowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","1750","EEHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","1750","EELowR9")
 
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","1750","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","1750","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","1750","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","1750","EELowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","2250","EBHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","2250","EBLowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","2250","EEHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","2250","EELowR9")
 
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","2000","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","2000","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","2000","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","2000","EELowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","2500","EBHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","2500","EBLowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","2500","EEHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","2500","EELowR9")
 
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","2250","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","2250","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","2250","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","2250","EELowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","2750","EBHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","2750","EBLowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","2750","EEHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","2750","EELowR9")
 
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","2500","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","2500","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","2500","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","2500","EELowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","3500","EBHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","3500","EBLowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","3500","EEHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","3500","EELowR9")
 
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","2750","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","2750","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","2750","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","2750","EELowR9")
-
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","3000","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","3000","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","3000","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","3000","EELowR9")
-
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","3500","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","3500","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","3500","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","3500","EELowR9")
-
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","4000","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","4000","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","4000","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","4000","EELowR9")
-
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","4500","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","4500","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","4500","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","4500","EELowR9")
-
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","5000","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","5000","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","5000","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","5000","EELowR9")
-
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","500","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","500","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","500","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","500","EELowR9")
-
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","750","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","750","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","750","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","01","750","EELowR9")
-
-
-]
-
-
-
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","1000","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","1000","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","1000","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","1000","EELowR9")
-
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","1500","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","1500","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","1500","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","1500","EELowR9")
-
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","2000","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","2000","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","2000","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","2000","EELowR9")
-
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","3000","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","3000","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","3000","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","3000","EELowR9")
-
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","4000","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","4000","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","4000","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","4000","EELowR9")
-
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","5000","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","5000","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","5000","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","5000","EELowR9")
-
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","500","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","500","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","500","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","500","EELowR9")
-
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","750","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","750","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","750","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","001","750","EELowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","4500","EBHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","4500","EBLowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","4500","EEHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","01","4500","EELowR9")
 
 
 
 
 
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","1000","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","1000","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","1000","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","1000","EELowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","1500","EBHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","1500","EBLowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","1500","EEHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","1500","EELowR9")
 
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","1500","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","1500","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","1500","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","1500","EELowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","2000","EBHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","2000","EBLowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","2000","EEHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","2000","EELowR9")
 
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","2000","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","2000","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","2000","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","2000","EELowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","3000","EBHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","3000","EBLowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","3000","EEHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","3000","EELowR9")
 
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","3000","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","3000","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","3000","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","3000","EELowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","4000","EBHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","4000","EBLowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","4000","EEHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","4000","EELowR9")
 
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","4000","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","4000","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","4000","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","4000","EELowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","5000","EBHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","5000","EBLowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","5000","EEHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","5000","EELowR9")
 
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","5000","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","5000","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","5000","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","5000","EELowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","500","EBHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","500","EBLowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","500","EEHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","500","EELowR9")
 
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","500","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","500","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","500","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","500","EELowR9")
-
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","750","EBHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","750","EBLowR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","750","EEHighR9")
-signalModelFormat("dataCommonV6/output.root","cicGenIso","02","750","EELowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","750","EBHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","750","EBLowR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","750","EEHighR9")
+signalModelFormat("dataCommon76x_v2/output.root","cicGenIso","001","750","EELowR9")
 
 .q
 
