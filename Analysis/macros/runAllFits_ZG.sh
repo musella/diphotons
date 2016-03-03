a046242148577117cc19598f245307a72f28effe
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2016-03-03 13:38:18 +0100

    using bias and fwhm

--- Analysis/macros/runAllFits_ZG.sh
+++ Analysis/macros/runAllFits_ZG.sh
@@ -1,25 +1,27 @@
 ## parallel --ungroup -j 1 './combine_maker.sh full_analysis_spring15_7415v2_sync_v6 --data-file ~/public/workspace/exo/full_analysis_spring15_7415v2_sync_v5_data_ecorr/output.root --lumi 2.4 --fit-name cic2 --plot-fit-bands --rescale-signal-to 1e-3  --parametric-signal ~/eos/cms/store/user/crovelli/WSdiphotonConSmearings/nominalWSwithSmear_k{}_m1000to4900.root --parametric-signal ~/eos/cms/store/user/crovelli/WSdiphotonConSmearings/nominalWSwithSmear_k{}_m500to998.root  --parametric-signal-xsection xsections.json --parametric-signal-acceptance  acceptance_pu.json --load lumi.json  --compute-fwhm --generate-ws-bkgnbias --only-coups {} --label approval --minos-bands' ::: 001 005 007 01 015 02
 
-./combine_maker_ZG.sh EventYields_v0_silver --luminosity 2.6 \
+./combine_maker_ZG.sh EventYields_v0_eth74X --luminosity 2.7 \
     --fit-name ${1:-fit_em} \
     --fit-background \
-    --mc-data \
-    --label combination_pcorr --load combine_maker_ZG.json \
+    --minos-bands \
+    --plot-fit-bands  \
+    --label combination_pcorr \
+    --load combine_maker_ZG.json \
     --redo-input \
     --generate-signal-dataset \
     --doubleCB-signal \
+    --signal-width 5p6 \
     --generate-datacard \
-    --saveas png,pdf,eps \
+    --generate-ws-bkgnbias \
+    --compute-fwhm \
+    --saveas png,pdf,eps,root \
     --signal-scalefactor-forpdf 1
 
-#./combine_maker_ZG.sh EventYields_v0 --luminosity 2.3 \
+    #--plot-blind 500,3000 \
+    #--mc-data \
+    #--fast-bands \
     #--gaussian-signal \
     #--no-use-custom-pdfs \
-    #--minos-bands \
-    #--plot-fit-bands  \
-    #--no-use-custom-pdfs \
-    #--minos-bands \
-    #--plot-fit-bands  \
 
 ### parallel --ungroup './splitallcards.sh full_analysis_spring15_7415v2_sync_v5_data_ecorr_cic2_default_shapes_approval_lumi_2.4 {}' ::: EBEB EBEE
 
