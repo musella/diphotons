cd87fcbdc2015306a0ee47ccf7f30088f82fbf28
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2016-03-03 16:21:11 +0100

    new range up to 2500

--- Analysis/macros/run_toy_ZG.sh
+++ Analysis/macros/run_toy_ZG.sh
@@ -31,7 +31,8 @@ $mydir/bkg_bias.py --n-toys $ntoys \
     --fit-name fit_v0 \
     --saveas png \
     --test-range 200,220 --test-range 220,240 --test-range 240,260 --test-range 260,300 --test-range 300,350 --test-range 350,400 --test-range 400,450 --test-range 450,500 \
-    --test-range 1000,1200 --test-range 1200,1800 --test-range 1800,2500 --test-range 2500,3500 --test-range 3500,4500 --test-range 500,550 --test-range 550,600 --test-range 600,650 --test-range 650,700 --test-range 700,750 --test-range 750,800 --test-range 800,900 --test-range 900,1000 --test-range 4500,5500 --test-range 5500,10000 --first-toy $toy \
+    --test-range 1000,1200 --test-range 1200,1800 --test-range 1800,2500 --test-range 500,550 --test-range 550,600 --test-range 600,650 --test-range 650,700 \
+    --test-range 700,750 --test-range 750,800 --test-range 800,900 --test-range 900,1000 --first-toy $toy \
     $@
 
 
