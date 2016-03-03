2f3ea253fd832657aa0ff4708bf6ccf46956cf6c
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2016-03-03 14:03:21 +0100

    first commit

---
+++ Analysis/macros/run_all_bkgBias_ZG.sh
@@ -0,0 +1,6 @@
+source runBkgBias_ZG.sh expow2
+source runBkgBias_ZG.sh invpow
+source runBkgBias_ZG.sh moddijet
+source runBkgBias_ZG.sh invpowlin
+source runBkgBias_ZG.sh sumexp3
+source runBkgBias_ZG.sh pow
