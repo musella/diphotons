1aef1ee6b02a5d8ea134f6592dd8b94be6bb5404
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2016-03-08 11:05:19 +0100

    200_2500

--- Analysis/macros/analyzeToys.sh
+++ Analysis/macros/analyzeToys.sh
@@ -1,3 +1,3 @@
 ./hadd_toys.sh $1
-./bkg_bias.py --analyze-bias --bias-files $1/toys.root --bias-labels 200_10000 -O $1 --no-use-custom
+./bkg_bias.py --analyze-bias --bias-files $1/toys.root --bias-labels 200_2500 -O $1 --no-use-custom
 
