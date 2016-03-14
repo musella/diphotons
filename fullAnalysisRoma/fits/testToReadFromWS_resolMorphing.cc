34d98c01f2abe9095dae7f27a40b744a3671cdaa
crovelli <crovelli@gmail.com>
2016-03-14 10:48:05 +0100

    final version

--- fullAnalysisRoma/fits/testToReadFromWS_resolMorphing.cc
+++ fullAnalysisRoma/fits/testToReadFromWS_resolMorphing.cc
@@ -27,29 +27,27 @@ using namespace std;
 
 void runfits() {
 
-  TFile *fileResNominal     = new TFile("daTenereVersioneFinale_smearings/ResolutionHistos_nominalSmearings.root");
-  TFile *filePointFromMorph = new TFile("daTenereVersioneFinale_smearings/ResHistosGenOnlyScan_nominalSmearings.root");
+  TFile *fileResNominal     = new TFile("daTenere/resol38T/2classes/ResolutionHistos_1000to5000.root");
+  TFile *filePointFromMorph = new TFile("daTenere/resol38T/2classes/ResHistosGenOnlyScan_1000to5000.root");
 
   RooRealVar* deltaM = new RooRealVar("deltaM", "",   -10000, 10000,   "GeV");
 
   // Comparing couplings
-  RooDataHist *resolRDH_mass1000_catEBEE_nom   = (RooDataHist*)fileResNominal->Get("resolRDH_mass1000_catEBEE");
-  RooDataHist *resolRDH_mass1000_catEBEE_morph = (RooDataHist*)filePointFromMorph->Get("resolRDH_mass1000_catEBEE");
-  RooDataHist *resolRDH_mass996_catEBEE_morph  = (RooDataHist*)filePointFromMorph->Get("resolRDH_mass996_catEBEE");
-  RooDataHist *resolRDH_mass992_catEBEE_morph  = (RooDataHist*)filePointFromMorph->Get("resolRDH_mass992_catEBEE");
-  RooDataHist *resolRDH_mass1004_catEBEE_morph = (RooDataHist*)filePointFromMorph->Get("resolRDH_mass1004_catEBEE");
-  RooDataHist *resolRDH_mass1008_catEBEE_morph = (RooDataHist*)filePointFromMorph->Get("resolRDH_mass1008_catEBEE");
-  RooDataHist *resolRDH_mass1500_catEBEE_morph = (RooDataHist*)filePointFromMorph->Get("resolRDH_mass1500_catEBEE");
+  RooDataHist *resolRDH_mass1000_catEBEB_nom   = (RooDataHist*)fileResNominal->Get("resolRDH_mass1000_catEBEB");
+  RooDataHist *resolRDH_mass1000_catEBEB_morph = (RooDataHist*)filePointFromMorph->Get("resolRDH_mass1000_catEBEB");
+  RooDataHist *resolRDH_mass746_catEBEB_morph  = (RooDataHist*)filePointFromMorph->Get("resolRDH_mass746_catEBEB");
+  RooDataHist *resolRDH_mass742_catEBEB_morph  = (RooDataHist*)filePointFromMorph->Get("resolRDH_mass742_catEBEB");
+  RooDataHist *resolRDH_mass754_catEBEB_morph = (RooDataHist*)filePointFromMorph->Get("resolRDH_mass754_catEBEB");
+  RooDataHist *resolRDH_mass758_catEBEB_morph = (RooDataHist*)filePointFromMorph->Get("resolRDH_mass758_catEBEB");
 
   RooPlot *frameA = deltaM->frame(Range(-100,100),Bins(50));
   TCanvas *c1 = new TCanvas("c1","c1",1);
-  resolRDH_mass1000_catEBEE_nom->plotOn(frameA, LineColor(kBlack), MarkerStyle(kSolid), MarkerSize(3),Rescale(1./resolRDH_mass1000_catEBEE_nom->sumEntries()));
-  resolRDH_mass1000_catEBEE_morph->plotOn(frameA, LineColor(kRed), MarkerStyle(kDashed), MarkerSize(3),Rescale(1./resolRDH_mass1000_catEBEE_morph->sumEntries()));
-  resolRDH_mass996_catEBEE_morph->plotOn(frameA, LineColor(kBlue), MarkerStyle(kDashed), MarkerSize(3),Rescale(1./resolRDH_mass996_catEBEE_morph->sumEntries()));
-  resolRDH_mass992_catEBEE_morph->plotOn(frameA, LineColor(kYellow), MarkerStyle(kDashed), MarkerSize(3),Rescale(1./resolRDH_mass992_catEBEE_morph->sumEntries()));
-  resolRDH_mass1004_catEBEE_morph->plotOn(frameA, LineColor(kOrange), MarkerStyle(kDashed), MarkerSize(3),Rescale(1./resolRDH_mass1004_catEBEE_morph->sumEntries()));
-  resolRDH_mass1008_catEBEE_morph->plotOn(frameA, LineColor(kGreen), MarkerStyle(kDashed), MarkerSize(3),Rescale(1./resolRDH_mass1008_catEBEE_morph->sumEntries()));
-  //resolRDH_mass1500_catEBEE_morph->plotOn(frameA, LineColor(kPink), MarkerStyle(kDashed), MarkerSize(3),Rescale(1./resolRDH_mass1500_catEBEE_morph->sumEntries()));
+  resolRDH_mass1000_catEBEB_nom->plotOn(frameA, LineColor(kBlack), MarkerStyle(kSolid), MarkerSize(3),Rescale(1./resolRDH_mass1000_catEBEB_nom->sumEntries()));
+  resolRDH_mass1000_catEBEB_morph->plotOn(frameA, LineColor(kRed), MarkerStyle(kDashed), MarkerSize(3),Rescale(1./resolRDH_mass1000_catEBEB_morph->sumEntries()));
+  //resolRDH_mass746_catEBEB_morph->plotOn(frameA, LineColor(kBlue), MarkerStyle(kDashed), MarkerSize(3),Rescale(1./resolRDH_mass746_catEBEB_morph->sumEntries()));
+  //resolRDH_mass742_catEBEB_morph->plotOn(frameA, LineColor(kYellow), MarkerStyle(kDashed), MarkerSize(3),Rescale(1./resolRDH_mass742_catEBEB_morph->sumEntries()));
+  //resolRDH_mass754_catEBEB_morph->plotOn(frameA, LineColor(kOrange), MarkerStyle(kDashed), MarkerSize(3),Rescale(1./resolRDH_mass754_catEBEB_morph->sumEntries()));
+  //resolRDH_mass758_catEBEB_morph->plotOn(frameA, LineColor(kGreen), MarkerStyle(kDashed), MarkerSize(3),Rescale(1./resolRDH_mass758_catEBEB_morph->sumEntries()));
 
   c1->SetLogy();
   frameA->Draw();
