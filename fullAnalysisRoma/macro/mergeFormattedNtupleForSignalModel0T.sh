4404bde9feb261148258e961375706cb3d0bdfd8
crovelli <crovelli@gmail.com>
2016-03-14 10:37:12 +0100

    formatting ntuples, more versions

---
+++ fullAnalysisRoma/macro/mergeFormattedNtupleForSignalModel0T.sh
@@ -0,0 +1,36 @@
+#! /bin/sh                                                                                                                   
+
+hadd /tmp/crovelli/FormSigMod_kpl01_M1000.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M1000_catEBHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M1000_catEBLowR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M1000_catEEHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M1000_catEELowR9.root
+
+hadd /tmp/crovelli/FormSigMod_kpl01_M1250.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M1250_catEBHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M1250_catEBLowR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M1250_catEEHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M1250_catEELowR9.root
+
+hadd /tmp/crovelli/FormSigMod_kpl01_M1500.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M1500_catEBHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M1500_catEBLowR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M1500_catEEHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M1500_catEELowR9.root
+
+hadd /tmp/crovelli/FormSigMod_kpl01_M1750.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M1750_catEBHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M1750_catEBLowR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M1750_catEEHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M1750_catEELowR9.root
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M2000.root /tmp/crovelli/FormSigMod_cicGenIso_kpl001_M2000_catEBHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl001_M2000_catEBLowR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl001_M2000_catEEHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl001_M2000_catEELowR9.root
+
+hadd /tmp/crovelli/FormSigMod_kpl01_M2250.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M2250_catEBHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M2250_catEBLowR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M2250_catEEHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M2250_catEELowR9.root
+
+hadd /tmp/crovelli/FormSigMod_kpl01_M2500.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M2500_catEBHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M2500_catEBLowR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M2500_catEEHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M2500_catEELowR9.root
+
+hadd /tmp/crovelli/FormSigMod_kpl01_M2750.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M2750_catEBHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M2750_catEBLowR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M2750_catEEHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M2750_catEELowR9.root
+
+hadd /tmp/crovelli/FormSigMod_kpl01_M3000.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M3000_catEBHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M3000_catEBLowR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M3000_catEEHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M3000_catEELowR9.root
+
+hadd /tmp/crovelli/FormSigMod_kpl01_M3500.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M3500_catEBHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M3500_catEBLowR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M3500_catEEHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M3500_catEELowR9.root
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M4000.root /tmp/crovelli/FormSigMod_cicGenIso_kpl001_M4000_catEBHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl001_M4000_catEBLowR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl001_M4000_catEEHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl001_M4000_catEELowR9.root
+
+hadd /tmp/crovelli/FormSigMod_kpl01_M4500.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M4500_catEBHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M4500_catEBLowR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M4500_catEEHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M4500_catEELowR9.root
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M750.root /tmp/crovelli/FormSigMod_cicGenIso_kpl001_M750_catEBHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl001_M750_catEBLowR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl001_M750_catEEHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl001_M750_catEELowR9.root
+
+hadd /tmp/crovelli/FormSigMod_kpl001_M500.root /tmp/crovelli/FormSigMod_cicGenIso_kpl001_M500_catEBHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl001_M500_catEBLowR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl001_M500_catEEHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl001_M500_catEELowR9.root
+
+hadd /tmp/crovelli/FormSigMod_kpl01_M5000.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M5000_catEBHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M5000_catEBLowR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M5000_catEEHighR9.root /tmp/crovelli/FormSigMod_cicGenIso_kpl01_M5000_catEELowR9.root
+
+
+rm /tmp/crovelli/FormSigMod_cic*_kpl001_*
+rm /tmp/crovelli/FormSigMod_cic*_kpl01_*
+rm /tmp/crovelli/FormSigMod_cic*_kpl02_*
